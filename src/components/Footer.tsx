9af1c16c8e5a96c4a7c0d603c838fca5e851bb91
Harrison Clements <111847640+H-E-Clements@users.noreply.github.com>
2026-02-13 15:51:12 +0000

    Update Footer.tsx
    Added correct facebook link.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,7 +27,7 @@ function Footer() {
           <ul className="mt-6 flex space-x-4">
             {/* Facebook */}
             <li>
-              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hover:opacity-80 transition-opacity">
+              <a href="https://www.facebook.com/rungtawanauthenticthaitherapy/" target="_blank" rel="noopener noreferrer" className="hover:opacity-80 transition-opacity">
                 <svg xmlns="http://www.w3.org/2000/svg" className="fill-blue-500 w-7 h-7" viewBox="0 0 49.652 49.652">
                   <path d="M24.826 0C11.137 0 0 11.137 0 24.826c0 13.688 11.137 24.826 24.826 24.826 13.688 0 24.826-11.138 24.826-24.826C49.652 11.137 38.516 0 24.826 0zM31 25.7h-4.039v14.396h-5.985V25.7h-2.845v-5.088h2.845v-3.291c0-2.357 1.12-6.04 6.04-6.04l4.435.017v4.939h-3.219c-.524 0-1.269.262-1.269 1.386v2.99h4.56z" />
                 </svg>
